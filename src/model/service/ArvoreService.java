7631cf2f22dafabd125f300765ba49b2f1c38eea
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-09-24 12:10:23 -0300

    Lógica das árvores binárias (simple e balanceada) feita

--- src/model/service/ArvoreService.java
+++ src/model/service/ArvoreService.java
@@ -1,4 +1,29 @@
 package model.service;
 
+import model.ArvoreBalanceada;
+import model.ArvoreBinaria;
+import model.Node;
+
 public class ArvoreService {
+    private ArvoreBinaria arvoreBinaria;
+    private ArvoreBalanceada arvoreBalanceada;
+
+    public ArvoreService() {
+        arvoreBinaria = new ArvoreBinaria();
+        arvoreBalanceada = new ArvoreBalanceada();
+    }
+
+    public void inserir(int data, boolean balanceada) {
+        if (balanceada) arvoreBalanceada.inserir(data);
+        else arvoreBinaria.inserir(data);
+    }
+
+    public void remover(int data, boolean balanceada) {
+        if (balanceada) arvoreBalanceada.remover(data);
+        else arvoreBinaria.remover(data);
+    }
+
+    public Node getRaiz(boolean balanceada) {
+        return balanceada ? arvoreBalanceada.getRaiz() : arvoreBinaria.getRaiz();
+    }
 }
